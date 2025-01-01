ef7c4a04b5a6495ea43a92a97ae50bda3e51d4ef
CalebAkomaye <calebakomaye@outlook.com>
2025-01-01 21:13:12 +0100

    📦 NEW: configure webhooks with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ConvexClientProvider } from '@/providers/ConvexClientProvider';
+import Navbar from '@/components/navbar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClientProvider>
+          <div className='absolute top-0 -z-10 h-full w-full bg-white'>
+            <div className='absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]'></div>
+          </div>
+          <Navbar />
+          {children}
+        </ConvexClientProvider>
       </body>
     </html>
   );
